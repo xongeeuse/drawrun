61ccacd8a4eefc238baa62c737003cda7f3ceb1b
unknown <winlouder3@gmail.com>
2025-01-23 12:17:13 +0900

    S12P11E208-58 [BE] init : 프로젝트 세팅

---
+++ backend/src/main/java/com/moovly/movely_backend/MovelyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moovly.movely_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovelyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovelyBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/moovly/movely_backend/MovelyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moovly.movely_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovelyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
