1b1a6075bc052ca79b0aec59758e280de131af48
송지영 <xixouxx@gmail.com>
2025-01-25 16:38:01 +0900

    S12P11E208-56 [FE] init : 프로젝트 세팅

---
+++ DrawRunAndroid/build.gradle.kts
@@ -0,0 +1,25 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    // Hilt 의존성 주입 플러그인
+    alias(libs.plugins.hilt) apply false
+    // Kotlin Annotation Processing Tool 플러그인
+    alias(libs.plugins.kotlin.kapt) apply false
+}
+
+// 빌드 스크립트 설정
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
+// 프로젝트 정리를 위한 사용자 정의 태스크 등록
+tasks.register<Delete>("clean") {
+    delete(layout.buildDirectory)  // 루트 프로젝트의 build 디렉토리 삭제
+}
