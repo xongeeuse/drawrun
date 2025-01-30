ff126bdda5f94ad29aa61a793b0857bfec673db9
unknown <winlouder3@gmail.com>
2025-01-30 18:15:41 +0900

    S12P11E208-87 [BE] test: cicd test

--- backend/src/main/java/com/dasima/drawrun/test.java
+++ backend/src/main/java/com/dasima/drawrun/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("왜 안됨??");
+        System.out.println("제발 되라");
     }
 }
