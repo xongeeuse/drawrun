a52607f5fa0af61254c22ce14e66b0a0d88c337b
unknown <winlouder3@gmail.com>
2025-01-30 21:27:43 +0900

    S12P11E208-87 [BE] test: test code5 for cicd

--- backend/src/main/java/com/dasima/drawrun/test.java
+++ backend/src/main/java/com/dasima/drawrun/test.java
@@ -10,5 +10,6 @@ public class test {
         System.out.println("제발 ㅠㅠ");
         System.out.println("Docker 아이디 설정 잘못함 ㅋㅋㅋㅋㅋ ㅠㅠ");
         System.out.println("아 3시간째 뭐하고있노;;");
+        System.out.println("mysql만 되면 완성ㅋ");
     }
 }
