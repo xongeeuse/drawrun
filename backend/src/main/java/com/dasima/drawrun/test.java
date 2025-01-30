e1857454461e785ec31b030a49cc474e9cb0d0cf
unknown <winlouder3@gmail.com>
2025-01-30 18:07:28 +0900

    S12P11E208-87 [BE] test: cicd test

--- backend/src/main/java/com/dasima/drawrun/test.java
+++ backend/src/main/java/com/dasima/drawrun/test.java
@@ -3,5 +3,6 @@ package com.dasima.drawrun;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("왜 안됨??");
     }
 }
