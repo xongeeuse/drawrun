9b7e0e98c50b2fcd004487c3e11ecb1909c78db8
SeonghyeonKim <kkgojn@gmail.com>
2025-01-31 15:50:11 +0900

    S12P11E208-82 [BE] feat : 로그인 기능 구현
    아이디를 통해 사용자 찾기, 사용자 코드를 통해 권한 찾기, 비밀번호 비교 기능 추가 작성

--- backend/src/main/java/com/dasima/drawrun/domain/user/service/UserServiceImpl.java
+++ backend/src/main/java/com/dasima/drawrun/domain/user/service/UserServiceImpl.java
@@ -9,12 +9,14 @@ import com.dasima.drawrun.domain.user.repository.RoleRepository;
 import com.dasima.drawrun.domain.user.repository.UserRepository;
 import com.dasima.drawrun.global.exception.CustomException;
 import com.dasima.drawrun.global.exception.ErrorCode;
+import com.dasima.drawrun.global.security.dto.response.AccessTokenInfoResponseDto;
+import com.dasima.drawrun.global.security.dto.response.TokenResponseDto;
 import com.dasima.drawrun.global.security.provider.TokenProvider;
+import com.dasima.drawrun.global.util.RedisUtils;
 import jakarta.transaction.Transactional;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -32,6 +34,7 @@ public class UserServiceImpl implements UserService {
   private final RoleRegisterRepository roleRegisterRepo;
   private final RoleRepository roleRepo;
   private final BCryptPasswordEncoder passwordEncoder;
+  private final RedisUtils redisUtils;
 
   @Override
   public Object register(RegisterRequestDto dto) {
@@ -64,6 +67,43 @@ public class UserServiceImpl implements UserService {
     return Map.of(
         "status", true
     );
+  }
+
+  private User findById(String id) {
+    return userRepo.findById(id).orElseThrow(() -> {
+      return new CustomException(ErrorCode.NOT_EXIST_MEMBER_ID);
+    });
+  }
+
+  private User findUserWithRoleNameByUserId(int userId) {
+    return userRepo.findUserWithRoleNameById(userId);
+  }
+
+  private void checkPassword(String password, User user) {
+    if (!passwordEncoder.matches(password, user.getUserPassword())) {
+      throw new CustomException(ErrorCode.INCORRECT_MEMBER_PASSWORD);
+    }
+  }
+
+  @Override
+  public TokenResponseDto login(String id, String password) {
+    // user 찾기
+    User user = findById(id);
+    User detailUser = findUserWithRoleNameByUserId(user.getUserId());
+    checkPassword(password, detailUser);
+
+    AccessTokenInfoResponseDto accessTokenInfoResponseDto = tokenProvider.createAccessToken(
+        detailUser);
+
+    TokenResponseDto tokenResponseDto = new TokenResponseDto();
+    tokenResponseDto.setAccessTokenInfoResponse(accessTokenInfoResponseDto);
+    tokenResponseDto.setRefreshTokenInfoResponse(
+        tokenProvider.createRefreshToken(detailUser, 604800 * 1000));
+
+    // refresh 토큰을 redis에 저장
+    redisUtils.setData(tokenResponseDto.getRefreshTokenInfoResponse(), id, (long) 604800 * 1000);
+    //refresh 토큰과 access token 두개를 발급한다.
+    return tokenResponseDto;
   }
 
 }
