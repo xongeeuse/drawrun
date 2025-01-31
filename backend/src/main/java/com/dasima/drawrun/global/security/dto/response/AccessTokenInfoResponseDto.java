1425605556655c9e72fc0134be68de24129f2f81
SeonghyeonKim <kkgojn@gmail.com>
2025-01-31 15:52:43 +0900

    S12P11E208-82 [BE] fix : body에는 엑세스 토큰만 전달하도록 변경

--- backend/src/main/java/com/dasima/drawrun/global/security/dto/response/AccessTokenInfoResponseDto.java
+++ backend/src/main/java/com/dasima/drawrun/global/security/dto/response/AccessTokenInfoResponseDto.java
@@ -1,6 +1,5 @@
 package com.dasima.drawrun.global.security.dto.response;
 
-import java.util.Date;
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
@@ -11,7 +10,7 @@ import lombok.ToString;
 public class AccessTokenInfoResponseDto {
   private String accessToken; // access token
 
-  private Date accessTokenExpireTime; // 토큰의 만료날
-  private String email; // 사용자 이메일
-  private String tokenId;
+  // private Date accessTokenExpireTime; // 토큰의 만료날
+  // private String email; // 사용자 이메일
+  // private String tokenId;
 }
