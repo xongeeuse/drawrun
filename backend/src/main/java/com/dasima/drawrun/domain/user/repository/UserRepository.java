c92dc6e09e4525101f43ff2ff823393055d6c87e
SeonghyeonKim <kkgojn@gmail.com>
2025-01-31 15:52:17 +0900

    S12P11E208-82 [BE] fix : 아이디로 사용자를 찾도록 변경

--- backend/src/main/java/com/dasima/drawrun/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/dasima/drawrun/domain/user/repository/UserRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-  Optional<User> findByUserEmail(String email);
+  Optional<User> findById(String id);
 
   boolean existsById(String id);
 
