a3ecaedc9595e111d8c24981a7ba70e113e18142
SeonghyeonKim <kkgojn@gmail.com>
2025-02-01 14:42:15 +0900

    S12P11E208-82 [BE] feat : 토큰 관련 에러코드 작성

--- backend/src/main/java/com/dasima/drawrun/global/exception/ErrorCode.java
+++ backend/src/main/java/com/dasima/drawrun/global/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
   DUPLICATE_MEMBER_NICKNAME(2003, "이미 사용중인 닉네임입니다"),
 
   NOT_EXIST_MEMBER_ID(2004, "존재하지 않는 아이디입니다."),
-  INCORRECT_MEMBER_PASSWORD(2005, "비밀번호가 일치하지 않습니다.");
+  INCORRECT_MEMBER_PASSWORD(2005, "비밀번호가 일치하지 않습니다."),
+
+  INVALID_REFRESH_TOKEN(2006, "유효하지 않는 토큰입니다.");
 
 
   private final int code;
