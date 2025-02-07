f5f713c19fd1139dc9284d849788e547e9399ed3
SeonghyeonKim <kkgojn@gmail.com>
2025-02-07 17:52:01 +0900

    S12P11E208-85 [BE] feat : 배지 관련 기능 구현

--- backend/src/main/java/com/dasima/drawrun/domain/mypage/service/BadgeService.java
+++ backend/src/main/java/com/dasima/drawrun/domain/mypage/service/BadgeService.java
@@ -1,7 +1,13 @@
 package com.dasima.drawrun.domain.mypage.service;
 
+import com.dasima.drawrun.domain.mypage.dto.UserBadgeDto;
+
+import java.util.List;
+
 public interface BadgeService {
 
-  
+    List<UserBadgeDto> grant(int userId);
+
+    List<UserBadgeDto> check(int userId);
 
 }
--- backend/src/main/java/com/dasima/drawrun/domain/mypage/service/BadgeServiceImpl.java
+++ backend/src/main/java/com/dasima/drawrun/domain/mypage/service/BadgeServiceImpl.java
@@ -1,19 +1,37 @@
 package com.dasima.drawrun.domain.mypage.service;
 
+import com.dasima.drawrun.domain.mypage.dto.BadgeInfoDto;
+import com.dasima.drawrun.domain.mypage.dto.UserBadgeDto;
+import com.dasima.drawrun.domain.mypage.entity.BadgeInventory;
+import com.dasima.drawrun.domain.mypage.repository.BadgeInventoryRepository;
+import com.dasima.drawrun.global.config.BadgeConfig;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class BadgeServiceImpl implements BadgeService {
 
   @Autowired
-  BadgeService badgeService;
+  BadgeConfig badgeConfig;
+
   @Autowired
   BadgeInventoryRepository badgeInventoryRepo;
 
   @Override
   public List<UserBadgeDto> grant(int userId) {
     return null;
+  }
+
   @Override
   public List<UserBadgeDto> check(int userId) {
     List<BadgeInventory> badgeInventoryList = badgeInventoryRepo.findBadgeInventoryByUserId(userId);
