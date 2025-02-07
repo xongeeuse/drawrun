6172ca4952e6cc70999c13c707b800cf12ed517b
SeonghyeonKim <kkgojn@gmail.com>
2025-02-07 17:52:13 +0900

    S12P11E208-85 [BE] feat : 배지 관련 Controller 작성

--- backend/src/main/java/com/dasima/drawrun/domain/mypage/controller/BadgeController.java
+++ backend/src/main/java/com/dasima/drawrun/domain/mypage/controller/BadgeController.java
@@ -1,12 +1,47 @@
 package com.dasima.drawrun.domain.mypage.controller;
 
+import com.dasima.drawrun.domain.mypage.dto.UserBadgeDto;
+import com.dasima.drawrun.domain.mypage.service.BadgeService;
+import com.dasima.drawrun.global.common.ApiResponseJson;
+import com.dasima.drawrun.global.security.UserPrinciple;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/badge")
 public class BadgeController {
 
-  
+    @Autowired
+    private BadgeService badgeService;
+
+    @PostMapping("/grant")
+    public ResponseEntity<ApiResponseJson> grant(@AuthenticationPrincipal UserPrinciple userPrinciple) {
+        List<UserBadgeDto> badgeList = badgeService.grant(userPrinciple.getUserId());
+
+        if (badgeList.isEmpty()) {
+            return ResponseEntity.ok(
+                    new ApiResponseJson(true, 200, "지급할 배지가 없습니다.", null)
+            );
+        }
+        return ResponseEntity.ok(
+                new ApiResponseJson(true, 200, "배지 지급을 완료했습니다.", badgeList)
+        );
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<ApiResponseJson> check(@AuthenticationPrincipal UserPrinciple userPrinciple) {
+        List<UserBadgeDto> badgeList = badgeService.check(userPrinciple.getUserId());
+
+        return ResponseEntity.ok(
+                new ApiResponseJson(true, 200, "보유한 배지를 불러왔습니다.", badgeList)
+        );
+    }
 
 }
