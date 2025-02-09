acfbdf4bcecb2677dbacadfd1b723f4739abe4d9
SeonghyeonKim <kkgojn@gmail.com>
2025-02-09 15:51:15 +0900

    S12P11E208-89 [BE] feat : 예외처리 추가

--- backend/src/main/java/com/dasima/drawrun/domain/course/service/CourseServiceImpl.java
+++ backend/src/main/java/com/dasima/drawrun/domain/course/service/CourseServiceImpl.java
@@ -3,6 +3,8 @@ package com.dasima.drawrun.domain.course.service;
 import com.dasima.drawrun.domain.course.dto.request.CourseResultSaveRequest;
 import com.dasima.drawrun.domain.course.entity.CourseResult;
 import com.dasima.drawrun.domain.course.repository.CourseRepository;
+import com.dasima.drawrun.global.exception.CustomException;
+import com.dasima.drawrun.global.exception.ErrorCode;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,20 +16,24 @@ public class CourseServiceImpl implements CourseService {
   public int courseResultSave(int UserPK, CourseResultSaveRequest resultDto) {
     new CourseResult();
 
-    courseRepository.save(CourseResult
-            .builder()
-            .distanceKm(resultDto.getDistance_km())
-            .timeS(resultDto.getTime_s())
-            .paceS(resultDto.getPace_s())
-            .calorie(resultDto.getCalorie())
-            .state(1)
-            .heartbeat(resultDto.getHeartbeat())
-            .runImgUrl(resultDto.getEndImgUrl())
-            .cadence(resultDto.getCadence())
-            .userId(UserPK)
-            .userPathId(resultDto.getPathId())
-            .build()
-    );
+    try {
+      courseRepository.save(CourseResult
+          .builder()
+          .distanceKm(resultDto.getDistance_km())
+          .timeS(resultDto.getTime_s())
+          .paceS(resultDto.getPace_s())
+          .calorie(resultDto.getCalorie())
+          .state(1)
+          .heartbeat(resultDto.getHeartbeat())
+          .runImgUrl(resultDto.getEndImgUrl())
+          .cadence(resultDto.getCadence())
+          .userId(UserPK)
+          .userPathId(resultDto.getPathId())
+          .build()
+      );
+    } catch (Exception e) {
+      throw new CustomException(ErrorCode.RESULT_SAVE_ERROR);
+    }
 
     return 1;
   }
