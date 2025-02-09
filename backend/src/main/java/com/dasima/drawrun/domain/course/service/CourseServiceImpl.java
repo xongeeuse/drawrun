5b7b018ec30c1a12c4384c0c1947fb445e60712d
SeonghyeonKim <kkgojn@gmail.com>
2025-02-09 14:07:30 +0900

    S12P11E208-89 [BE] feat : 완주 기록 저장 기능 구현

--- backend/src/main/java/com/dasima/drawrun/domain/course/service/CourseServiceImpl.java
+++ backend/src/main/java/com/dasima/drawrun/domain/course/service/CourseServiceImpl.java
@@ -1,15 +1,35 @@
 package com.dasima.drawrun.domain.course.service;
 
 import com.dasima.drawrun.domain.course.dto.request.CourseResultSaveRequest;
+import com.dasima.drawrun.domain.course.entity.CourseResult;
+import com.dasima.drawrun.domain.course.repository.CourseRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
+  private CourseRepository courseRepository;
+
   @Override
   public int courseResultSave(int UserPK, CourseResultSaveRequest resultDto) {
+    new CourseResult();
 
+    courseRepository.save(CourseResult
+            .builder()
+            .distanceKm(resultDto.getDistance_km())
+            .timeS(resultDto.getTime_s())
+            .paceS(resultDto.getPace_s())
+            .calorie(resultDto.getCalorie())
+            .state(1)
+            .heartbeat(resultDto.getHeartbeat())
+            .runImgUrl(resultDto.getEndImgUrl())
+            .cadence(resultDto.getCadence())
+            .userId(UserPK)
+            .userPathId(resultDto.getPathId())
+            .build()
+    );
+
+    return 1;
   }
-
 
 }
