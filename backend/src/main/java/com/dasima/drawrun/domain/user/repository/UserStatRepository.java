009265d081cfd7af7bfe7a966ac826bdd00c2ff8
SeonghyeonKim <kkgojn@gmail.com>
2025-02-12 23:35:42 +0900

    S12P11E208-125 [BE] feat : userPK로 완주 기록 가져오는 함수 추가

--- backend/src/main/java/com/dasima/drawrun/domain/user/repository/UserStatRepository.java
+++ backend/src/main/java/com/dasima/drawrun/domain/user/repository/UserStatRepository.java
@@ -1,6 +1,11 @@
 package com.dasima.drawrun.domain.user.repository;
 
+import com.dasima.drawrun.domain.user.entity.UserStat;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserStatRepository extends JpaRepository<UserStat, Integer> {
+
+  List<UserStat> findByUserId(int userId);
+
 }
