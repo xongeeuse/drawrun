bc84d2aff8bb8db80f0808282d21d9326b9f32d5
SeonghyeonKim <kkgojn@gmail.com>
2025-02-12 23:35:15 +0900

    S12P11E208-125 [BE] feat : userPK로 User 찾는 함수 추가

--- backend/src/main/java/com/dasima/drawrun/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/dasima/drawrun/domain/user/repository/UserRepository.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
   Optional<User> findById(String id);
+  Optional<User> findByUserId(int userId);
   Optional<User> findByUserEmail(String email);
 
   Optional<User> findByUserEmailAndUserName(String email, String name);
