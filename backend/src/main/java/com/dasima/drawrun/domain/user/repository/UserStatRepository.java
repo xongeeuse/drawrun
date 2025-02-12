bd623daafe81e2212e5cd53a7a3bbf7e5b33db3f
SeonghyeonKim <kkgojn@gmail.com>
2025-02-12 17:56:31 +0900

    S12P11E208-125 [BE] feat : 사용자 기록 Repository 작성

---
+++ backend/src/main/java/com/dasima/drawrun/domain/user/repository/UserStatRepository.java
@@ -0,0 +1,6 @@
+package com.dasima.drawrun.domain.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserStatRepository extends JpaRepository<UserStat, Integer> {
+}
