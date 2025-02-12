8be74202daecc32ebc5bab6f508ad1ffb764562e
SeonghyeonKim <kkgojn@gmail.com>
2025-02-12 17:52:58 +0900

    S12P11E208-125 [BE] feat : 마이페이지 조회 Response 작성

---
+++ backend/src/main/java/com/dasima/drawrun/domain/user/dto/response/UserHistoryReponse.java
@@ -0,0 +1,16 @@
+package com.dasima.drawrun.domain.user.dto.response;
+
+import com.dasima.drawrun.domain.user.dto.HistoryDto;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class UserHistoryReponse {
+    int userPK;
+    String nickname;
+    String profileImgUrl;
+    List<HistoryDto> history;
+}
