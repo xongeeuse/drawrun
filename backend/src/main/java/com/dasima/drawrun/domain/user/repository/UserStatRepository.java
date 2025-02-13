7d186beeeb60335e2ad5762d585c063e5975d981
SeonghyeonKim <kkgojn@gmail.com>
2025-02-13 09:35:46 +0900

    S12P11E208-125 [BE] fix : 레포 명시

--- backend/src/main/java/com/dasima/drawrun/domain/user/repository/UserStatRepository.java
+++ backend/src/main/java/com/dasima/drawrun/domain/user/repository/UserStatRepository.java
@@ -1,9 +1,12 @@
 package com.dasima.drawrun.domain.user.repository;
 
 import com.dasima.drawrun.domain.user.entity.UserStat;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface UserStatRepository extends JpaRepository<UserStat, Integer> {
 
   List<UserStat> findByUserIdOrderByDateDesc(int userId);
