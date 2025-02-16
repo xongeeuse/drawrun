8a833c4aec762ae187dc223fefa12fa4d5c1f224
Yujiin <jiin4083@naver.com>
2025-02-16 18:00:41 +0900

    S12P11E208-149 [FE] feat: 하단 네비게이션 BaseActivity 설정 완

--- android/DrawRun/app/src/main/java/com/example/drawrun/ui/masterpiece/MasterpieceActivity.kt
+++ android/DrawRun/app/src/main/java/com/example/drawrun/ui/masterpiece/MasterpieceActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.drawrun.R
 import com.example.drawrun.databinding.ActivityMasterpieceBinding
+import com.example.drawrun.ui.common.BaseActivity
 import com.example.drawrun.ui.masterpiece.fragment.MasterpieceDetailFragment
 import com.example.drawrun.ui.masterpiece.fragment.MasterpieceSearchFragment
 
-class MasterpieceActivity : AppCompatActivity() {
+class MasterpieceActivity : BaseActivity() {
+    override fun getLayoutId(): Int = R.layout.activity_masterpiece  // ✅ 레이아웃 리소스 지정
     private lateinit var binding: ActivityMasterpieceBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
