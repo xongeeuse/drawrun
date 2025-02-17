64b0afad642f1c12f4439dcbd8e02a585f8e2d13
SeonghyeonKim <kkgojn@gmail.com>
2025-02-17 21:10:06 +0900

    S12P11E208-95 [BE] fix : 내부 서버로 요청을 보내도록 변경

--- backend/src/main/java/com/dasima/drawrun/domain/course/controller/AiCourseController.java
+++ backend/src/main/java/com/dasima/drawrun/domain/course/controller/AiCourseController.java
@@ -35,7 +35,7 @@ public class AiCourseController {
 
         // RestTemplate을 통해 FastAPI의 /api/v1/createMap 엔드포인트 호출
         RestTemplate restTemplate = new RestTemplate();
-        String fastApiUrl = "http://fastapi-container:8000/api/v1/createMap";
+        String fastApiUrl = "http://13.124.222.21:8000/api/v1/createMap";
 
         ResponseEntity<FastApiResponse> fastApiResponseEntity = restTemplate.postForEntity(
             fastApiUrl,
