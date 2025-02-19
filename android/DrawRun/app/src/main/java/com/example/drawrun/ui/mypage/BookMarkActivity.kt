dd201198d29f7d002815a804f5c2676abd901f59
skdud5126 <skdud5126@gmail.com>
2025-02-19 10:32:13 +0900

    git commit -m "S12P11E208-158 [FE] feat : 마이페이지(러닝히스토리 페이스 조절/ 북마크 하단바 추가)"

--- android/DrawRun/app/src/main/java/com/example/drawrun/ui/mypage/BookMarkActivity.kt
+++ android/DrawRun/app/src/main/java/com/example/drawrun/ui/mypage/BookMarkActivity.kt
@@ -1,14 +1,17 @@
 package com.example.drawrun.ui.mypage
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.example.drawrun.R
+import com.example.drawrun.ui.common.BaseActivity
 import com.example.drawrun.ui.mypage.fragment.BookMarkFragment
 
-class BookMarkActivity : AppCompatActivity() {
+class BookMarkActivity : BaseActivity() {
+    override fun getLayoutId(): Int = R.layout.activity_bookmark
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bookmark)
+        setupBottomNavigation()
 
         // ✅ 만약 Fragment가 동적으로 추가되지 않았다면 수동으로 추가
         if (savedInstanceState == null) {
