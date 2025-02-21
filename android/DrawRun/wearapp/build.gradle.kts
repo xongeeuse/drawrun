567107a9b1c20f6e54e9b6793b16d5b7d0618857
Yujiin <jiin4083@naver.com>
2025-02-21 09:26:44 +0900

    최종 수정

--- android/DrawRun/wearapp/build.gradle.kts
+++ android/DrawRun/wearapp/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.drawrun"
-        minSdk = 33
+        minSdk = 34
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
